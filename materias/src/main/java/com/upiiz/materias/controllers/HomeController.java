73a426f866d477f40568323d2bb568465d2caa0f
Luis Fernando Escobedo Hernandez <lescobedoh2400@alumno.ipn.mx>
2026-03-21 20:09:50 -0600

    Commit

---
+++ materias/src/main/java/com/upiiz/materias/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.upiiz.materias.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String redirigirAlListado() {
+        return "redirect:/materias";
+    }
+}
+
